0adb4968ecd9bb5c8ad5de648148a141392b76aa
Diego Marcos <diego.marcos@gmail.com>
2019-12-12 18:13:13 -0800

    Fix tests

--- src/components/hand-controls.js
+++ src/components/hand-controls.js
@@ -398,9 +398,9 @@ function getGestureEventName (gesture, active) {
 
 function isViveController (trackedControls) {
   var controller = trackedControls && trackedControls.controller;
-  var isVive = controller.id && controller.id.indexOf('OpenVR ') === 0 ||
+  var isVive = controller && (controller.id && controller.id.indexOf('OpenVR ') === 0 ||
     (controller.profiles &&
      controller.profiles[0] &&
-     controller.profiles[0] === 'htc-vive-controller-mv');
+     controller.profiles[0] === 'htc-vive-controller-mv'));
   return isVive;
 }
